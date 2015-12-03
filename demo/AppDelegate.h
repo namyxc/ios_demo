f93f99af0e62706fcc1124d612b6b277fb609f0f
MagNet Alma <magnetalma@magnet-mac-mini.local>
2015-12-03 15:24:59 +0100

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by MagNet Alma on 2015. 12. 03..
+//  Copyright © 2015. MagNet Alma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
